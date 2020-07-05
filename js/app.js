66ee3dcbb45acd91b94c9927e53cfe7613f885e7
Segun Dairo <segundairo@gmail.com>
2020-07-05 11:56:13 +0100

    completed UI functionality with comment included in app file

--- js/app.js
+++ js/app.js
@@ -3,17 +3,22 @@ const container = document.querySelector(".container");
 let standard;
 let premium;
 let selected;
+let blockedSeats=[];
 
 function showUI() {
-  const blockedSeats = JSON.parse(localStorage.getItem("selectedSeats"))
-  if (blockedSeats !== null && blockedSeats.length > 0) {
-    blockedSeats.forEach((seat) => {
-      seats[seat].classList.add('blocked')    
-   })  
+  //shows blocked seats in red and loads it into a variable from local storage
+  blockedSeats = JSON.parse(localStorage.getItem("selectedSeats"))
+  if (blockedSeats !== null && blockedSeats.length > 0) {   
+     blockedSeats.forEach((seat) => {
+       seats[seat].classList.add('blocked')    
+    })  
+  } else {
+    blockedSeats = []
   }
 }
 
 function showSeatStat() {
+  // function computes total seat selected by type and grand total
   // const blocked = document.querySelectorAll('.seats__container > div > *:not(.blocked)')
   // const blocked = document.querySelectorAll('.seats__container > div:not(:first-of-type) > *:not(.blocked)')
   selected = document.querySelectorAll(".selected");
@@ -29,34 +34,37 @@ function showSeatStat() {
 }
 
 function showSelectedSeats() {
+  // This function displays seat number using the combinaion of row number and seat number and updates local storage
   const seatOutput = document.querySelector("#seat__no ");
-  
+  seatOutput.innerHTML = '' 
   const seatIndex = [...selected].map((seat) => [...seats].indexOf(seat))
   const seatNumbers = [...selected].map((seat) => {
     const seatAlpha = seat.innerText;
     const seatCol = document.querySelectorAll(
       `.${seatAlpha.toLowerCase()} > *`)
-    const row = [...seatCol].indexOf(seat) + 1;
+    const row = [...seatCol].indexOf(seat) + 1; 
     return `${seatAlpha}${row}`;
  
 });
     
-   console.log(seatIndex);
-   
-  localStorage.setItem("selectedSeats", JSON.stringify(seatIndex))
-  
-  seatOutput.innerHTML = `<p class='seat__no'>${seatNumbers.join(", ")}</p>`;
+ 
+  if (!seatIndex !== null && seatIndex.length > 0) {
+// this snippet updates local storage by concatenating items previously stored in the local storage with the newly selected items
+    localStorage.setItem("selectedSeats", JSON.stringify(seatIndex.concat(blockedSeats)))
+      seatOutput.innerHTML = `<p class='seat__no'>${seatNumbers.join(", ")}</p>`;
+  }
 }
 
 
 container.addEventListener("click", (e) => {
+  // event triggered only when a seat has not been blocked and click is a seat within the container
   const item = e.target;
 
   if (item.classList.contains("seat")) {
     item.classList.toggle("selected");
-    showSeatStat();
-    showSelectedSeats();       
+    showSeatStat(); //Shows seat type, seat total selected
+    showSelectedSeats();  //Shows seat number
   }
 });
 
-showUI()
+showUI() //populates UI and displays the blocked seats
